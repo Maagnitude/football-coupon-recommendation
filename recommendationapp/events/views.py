52b9cc1229add7fea7442cfe76b2f6357a5952fc
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-06-10 23:23:39 +0300

    fix: results of create_user and json response

--- recommendationapp/events/views.py
+++ recommendationapp/events/views.py
@@ -2,6 +2,7 @@ from flask import request, Blueprint
 from recommendationapp.funcs import create_event, find_event, find_all_events
 from recommendationapp import db
 from sqlalchemy.orm import sessionmaker
+import json
 
 events = Blueprint('events', __name__)
 
@@ -9,15 +10,12 @@ events = Blueprint('events', __name__)
 def register_event():
     Session = sessionmaker(bind=db.engine)
     batch_session = Session()
-    json = request.json
-    result = create_event(json, batch_session)  
+    json_data = json.loads(request.json)['events']
+    print(json_data)
+    result = create_event(json_data, batch_session)  
     batch_session.close()
     
-    if isinstance(result, tuple):
-        status_code = result[-1]
-        return json, status_code
-    else:
-        return json, 200
+    return {"result": result[-1]}
 
 @events.route('/get_event')
 def get_event():
