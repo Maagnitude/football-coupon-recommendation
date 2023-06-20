7eb562df4e74e51052da69aa41bcc2de044251d0
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-06-20 17:18:26 +0300

    feat: loader starts with the app

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from recommendationapp import app
+import subprocess
+
+# Run the loader.py script
+@app.before_first_request
+def run_loader():
+    subprocess.Popen(['python', 'recommendationapp/teams/loader.py'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
