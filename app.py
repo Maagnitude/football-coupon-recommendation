7727b5087ff8f269b96d7d2f0a4a6d63e44a5d42
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-06-22 23:03:07 +0300

    bug: docker-compose.yml doesn't work

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from recommendationapp import app
 import subprocess
+from template_filters.filters import jinja2_enumerate, get_odds, get_event_participants, multiply
 
 # Run the loader.py script
 @app.before_first_request
 def run_loader():
     subprocess.Popen(['python', 'recommendationapp/teams/loader.py'])
+    
+app.add_template_filter(jinja2_enumerate)
+app.add_template_filter(get_odds)
+app.add_template_filter(get_event_participants)
+app.add_template_filter(multiply)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
